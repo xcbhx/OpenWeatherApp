2ed61800d39f04432992698ff96de2d7d813801a
Ceina Ellison <ceina.ellison@gmail.com>
2025-04-15 15:41:14 -0400

    created input form, added radio buttons to select temperature units

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,64 @@
-
+import { useState } from "react"; 
 
 function Weather() {
+  const [userInput, setUserInput] = useState("");
+  const [unit, setUnit] = useState("")
+
+
   return (
-    <div><h1>Weather Updates</h1></div>
+    <div>
+      <form>
+        <label htmlFor="zip">Zip Code</label>
+          <input 
+          id="zip"
+          type="number" 
+          placeholder="12345"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          />
+
+        {/* Temperatue Units */}
+        <fieldset>
+          <legend>Select Temperature Units:</legend>
+
+          <label htmlFor="metric">
+            <input 
+            id="metric"
+            type="radio" 
+            value="Metric"
+            checked={unit === "Metric"}
+            onChange={(e) => setUnit(e.target.value)}
+            />
+            Metric
+          </label>
+
+          <label htmlFor="imperial">
+            <input 
+            id="imperial"
+            type="radio" 
+            value="Imperial"
+            checked={unit === "Imperial"}
+            onChange={(e) => setUnit(e.target.value)}
+            />
+            Imperial
+          </label>
+
+          <label htmlFor="standard">
+            <input 
+            id="standard"
+            type="radio" 
+            value="Standard"
+            checked={unit === "Standard"}
+            onChange={(e) => setUnit(e.target.value)}
+            />
+            Standard
+          </label>
+
+        </fieldset>
+      </form>
+      <p>Current Zip Code: {userInput}</p>
+      <p>Selected Unit: {unit}</p>
+    </div>
   )
 };
 
